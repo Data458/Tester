f1c00984ec802c187b0653cab8259aa64b78536b
James Vuniloaloa <105198199+DexyGoose@users.noreply.github.com>
2022-05-13 14:39:49 +1000

    asd

--- 1_Multiple3or5.cpp
+++ 1_Multiple3or5.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 // NEW COMMENT 
 // ADDITIONAL COMMENT
-// THIRD COMMENT
+// THIRD COMMENTs
+// FOURTH 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 // The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
